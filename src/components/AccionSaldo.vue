0ae0af6902050cc101f523e39788eb83fb3ce1cf
Jose Villanueva <lv428694@gmail.com>
2022-05-16 00:18:48 -0500

    update

--- src/components/AccionSaldo.vue
+++ src/components/AccionSaldo.vue
@@ -1,13 +1,22 @@
 <template>
-    <!-- this.$emit('accion') es una alternativa para activar la funcionalidad, pero no
+  <!-- this.$emit('accion') es una alternativa para activar la funcionalidad, pero no
     es la más recomendable-->
-  <button @click="this.$emit('accion')">{{ texto }}</button>
+  <button @click="accion" :disabled="desactivar">{{ texto }}</button>
 </template>
 
 <script>
 export default {
   props: {
     texto: String,
+    desactivar: {
+        type: Boolean,
+        default: false
+    }
+  },
+  methods: {
+    accion() {
+      this.$emit("accion");
+    },
   },
 };
 </script>
--- src/components/Cuenta.vue
+++ src/components/Cuenta.vue
@@ -1,19 +1,14 @@
 <template>
-    <h2>Tipo de cuenta: {{tipo}}</h2>
-    <h2>Saldo: {{saldo}}</h2>
-    <h2>Cuenta {{estado ? 'Activa' : 'Desactivada'}}</h2>
-    <div v-for="(servicio, index) in servicios" :key="index">
-        {{index + 1}} .- {{servicio}}
-    </div> 
-    <AccionSaldo 
-        texto = 'Aumentar saldo'
-        @accion = 'aumentar'
-    />
-    <AccionSaldo 
-        texto = 'Disminuir saldo'
-    />
+  <h2>Tipo de cuenta: {{ tipo }}</h2>
+  <h2>Saldo: {{ saldo }}</h2>
+  <h2>Cuenta {{ estado ? "Activa" : "Desactivada" }}</h2>
+  <div v-for="(servicio, index) in servicios" :key="index">
+    {{ index + 1 }} .- {{ servicio }}
+  </div>
+  <AccionSaldo texto="Aumentar saldo" @accion="aumentar" />
+  <AccionSaldo texto="Disminuir saldo" @accion="disminuir" :desactivar = 'desactivar'/>
 
-    <!--
+  <!--
 @accion = 'aumentar'
 El componente hijo
  aumentar() {
@@ -25,33 +20,38 @@ El componente hijo
 </template>
 
 <script>
-
-import AccionSaldo from './AccionSaldo.vue'
+import AccionSaldo from "./AccionSaldo.vue";
 
 export default {
-    data() {
-        return {
-            saldo: 1000,
-            tipo: 'Visa',
-            estado: true,
-            servicios: ['giro', 'abono', 'transferencia']
-        }
+  data() {
+    return {
+      saldo: 1000,
+      tipo: "Visa",
+      estado: true,
+      servicios: ["giro", "abono", "transferencia"],
+      desactivar: false
+    };
+  },
+  components: {
+    AccionSaldo,
+  },
+  methods: {
+    //estas opciones se deberán de activar a través del componente hijo
+    aumentar() {
+      this.saldo = this.saldo + 100;
+      this.desactivar = false
     },
-    components: {
-        AccionSaldo
+    disminuir() {
+      if (this.saldo == 0) {
+        alert("saldo agotado");
+        this.desactivar = true
+      } else {
+        this.saldo = this.saldo - 100;
+      }
     },
-    methods: {
-        //estas opciones se deberán de activar a través del componente hijo
-        aumentar() {
-            this.saldo = this.saldo + 100;
-        },
-        disminuir() {
-            this.saldo = this.saldo - 100
-        }
-    }
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
