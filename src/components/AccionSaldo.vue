fea2667140386d90626476704a7cc9e991bd8fb0
Jose Villanueva <lv428694@gmail.com>
2022-05-16 00:02:13 -0500

    Update AccionSaldo.vue

--- src/components/AccionSaldo.vue
+++ src/components/AccionSaldo.vue
@@ -1,5 +1,7 @@
 <template>
-  <button>{{ texto }}</button>
+    <!-- this.$emit('accion') es una alternativa para activar la funcionalidad, pero no
+    es la más recomendable-->
+  <button @click="this.$emit('accion')">{{ texto }}</button>
 </template>
 
 <script>
